2764e6fc3eb84a7fb82f478481f55f8cd720acef
23harris23 <99044593+23harris23@users.noreply.github.com>
2022-02-04 10:52:32 -0800

    Thermostat/Humidistat function
    Controls a heater and humidifier using 433 MHz signals

---
+++ thermostat.py
@@ -0,0 +1,37 @@
+from rfRemote import rfSend
+from time import sleep
+import Adafruit_DHT
+import sys
+
+DHTpin = 22
+heaterStatus = False
+humidifierStatus = False
+
+def thermostat(setTemperature, setHumidity):
+	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHTpin)
+	temperature = (temperature * 1.8) + 32
+	if temperature >= setTemperature:
+		rfSend(1)
+		heaterStatus = False
+	else:
+		rfSend(0)
+		heaterStatus = True
+	if humidity >= setHumidity:
+		rfSend(2)
+		humidifierStatus = False
+	else:
+		rfSend(3)
+		humidifierStatus = True
+	return temperature, humidity, heaterStatus, humidifierStatus
+
+if __name__ == "__main__":
+	while True:
+		try:
+			temperature, humidity, heaterStatus, humidifierStatus = thermostat(65, 50)
+			print(str(temperature) + " F\n" + str(humidity) + " RH\n" + "Heat PWR" + str(heaterStatus) + "\nHumidifier PWR:" + str(humidifierStatus))
+			sleep(5)
+		except KeyboardInterrupt:
+			rfSend(1)
+			rfSend(3)
+			print("Shutting down")
+			sys.exit()
