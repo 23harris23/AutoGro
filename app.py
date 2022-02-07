9db9b65b18ecfa3f1f4fa3cf3c8c12eea1ff865b
23harris23 <99044593+23harris23@users.noreply.github.com>
2022-02-06 17:56:25 -0800

    Thermostat is now controllable from flask site

--- app.py
+++ app.py
@@ -1,25 +1,72 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from time import sleep
 from thermostat import thermostat
 from rfRemote import rfSend
 import sys
 from multiprocessing import Process
-setTempertaure = 67
-setHumidity = 50
+
 app = Flask(__name__)
-@app.route("/")
+
+def readConf():
+	list = []
+	try:
+		with open('config.txt', 'r') as fileHandle:
+			for line in fileHandle:
+				currentPlace = line[:-1]
+				list.append(currentPlace)
+			return list
+			fileHandle.close()
+	except IOError:
+		defaultList = [75, 50]
+		writeConf(defaultList)
+		file.close()
+
+def writeConf(list):
+	with open('config.txt', 'w') as fileHandle:
+		for listitem in list:
+			fileHandle.write('%s\n' % listitem)
+		fileHandle.close()
+
+@app.route("/", methods=["GET", "POST"])
 def main_page():
-	temperature, humidity, heaterStatus, humidifierStatus = thermostat(setTempertaure, setHumidity)
-	return("</p>" + str(temperature) + " F\n" + str(humidity) + " RH\n" + "Heater PWR: " + str(heaterStatus) + "\nHumidifier PWR: " + str(humidifierStatus) + "</p>")
+	list = readConf()
+	setTemperature = int(list[0])
+	setHumidity = int(list[1])
+	temperature, humidity, heaterStatus, humidifierStatus = thermostat(setTemperature, setHumidity)
+	if request.method == 'POST':
+		list = readConf()
+		setTemperature = int(list[0])
+		setHumidity = int(list[1])
+		reqTemp = request.form['newTemperature']
+		reqHumidity = request.form['newHumidity']
+		if reqTemp == '':
+			pass
+		else:
+			setTemperature = int(reqTemp)
+		if reqHumidity == '':
+			pass
+		else:
+			setHumidity = int(reqHumidity)
+			print(reqHumidity)
+		list = [setTemperature, setHumidity]
+		writeConf(list)
+		return redirect(request.url)
+	elif request.method == 'GET': 
+		return render_template("homeTemplate.html", temperature=temperature, humidity=humidity, heaterStatus=heaterStatus, humidifierStatus=humidifierStatus, setHumidity=setHumidity, setTemperature=setTemperature)
+
 def startSite():
-	app.run(host='0.0.0.0' port=2000)
+	app.run(host='0.0.0.0', port=2000, debug=True)
 if __name__ == "__main__":
 	webServer = Process(target=startSite)
 	webServer.start()
 	while True:
 		try:
-			temperature, humidity, heaterStatus, humidifierStatus = thermostat(setTempertaure, setHumidity)
+			list = readConf()
+			setTemperature = int(list[0])
+			setHumidity = int(list[1])
+			temperature, humidity, heaterStatus, humidifierStatus = thermostat(setTemperature, setHumidity)
 			print(str(temperature) + " F\n" + str(humidity) + " RH\n" + "Heater PWR: " + str(heaterStatus) + "\nHumidifier PWR: " + str(humidifierStatus))
+			print(str(setTemperature) + " " + str(setHumidity))
 			sleep(3)
 		except KeyboardInterrupt:
 			webServer.terminate()
